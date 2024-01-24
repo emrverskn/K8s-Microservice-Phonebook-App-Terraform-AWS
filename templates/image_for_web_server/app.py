13d49c0cf3d011c869298c8d2ffa80fc2d6b6bd3
Elif Merve Eriskin <140524629+emrverskn@users.noreply.github.com>
2024-01-24 16:16:00 +0300

    Updated app.py

--- templates/image_for_web_server/app.py
+++ templates/image_for_web_server/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, render_template
 from flaskext.mysql import MySQL
 import os
 
-
 app = Flask(__name__)
 
 app.config['MYSQL_DATABASE_HOST'] = os.getenv('MYSQL_DATABASE_HOST')
