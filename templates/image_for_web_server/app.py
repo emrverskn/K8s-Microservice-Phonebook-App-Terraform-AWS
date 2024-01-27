9753b807884992bd7e855bdba204f963281c4a65
Elif Merve Eriskin <140524629+emrverskn@users.noreply.github.com>
2024-01-27 18:47:30 +0300

    Update app.py

--- templates/image_for_web_server/app.py
+++ templates/image_for_web_server/app.py
@@ -45,7 +45,7 @@ def insert_person(name, number):
     return f'Person {name.strip().title()} added to Phonebook successfully'
 
 
-# and returns text 
+
 def update_person(name, number):
     query = f"""
     SELECT * FROM phonebook WHERE name like '{name.strip().lower()}';
